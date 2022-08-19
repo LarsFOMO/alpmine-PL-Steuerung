ac2fe65d02860ad52b629bd5477de81bb52e04ab
LarsFOMO <90476796+LarsFOMO@users.noreply.github.com>
2022-08-19 11:02:24 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -12,6 +12,10 @@
 
 #define trigger PD3
 #define echo PB0
+#define ssr1 PC2
+#define ssr2 PC3
+
+void ssr_off(void);
 
 uint64_t timer1_Overflow = 0;
 
@@ -27,40 +31,32 @@ ISR(TIMER1_OVF_vect)
 
 int main(void)
 {
-///***********************************************************************************************************
-    DDRB = 0x00;
-    DDRC = 0x00;
-    DDRD = 0x38;                                            // 0b0011 1000
-   // PORTD = 0x04;
 
-    PORTB = 0x01;                                           //  Pull-Up = ON
+///***********************************************************************************************************
+/// INIT SETUP
+    DDRB = 0x00;                                            //  PB0 = Echo
+    DDRC = 0x0C;                                            //  PC0 = PT100; PC1 = Leitfähigkeit; PC2 = SSR1; PC3 = SSR2
+    DDRD = 0x38;                                            //  PD3 = Trigger; PD4 = LED1; PD5 = LED2
+
+    PORTB = 0x01;                                           //  PB0: Pull-Up = ON
     PORTD = 0x00;
 
-    char temperatur[] = "";
-    char distance_forOut[] = "";
     uint16_t pt100_temp;
     uint16_t leitfaehig = 0;
     long count = 0;
     uint16_t distance = 0;
-
-///***********************************************************************************************************
+    uint8_t ok = 0;
 
     TIMSK1 = (1<<TOIE1);                                    //  Timer1 Overflow Interrupt = ON
     TCCR1A = 0;
 
     init_usart();
     sei();
+///***********************************************************************************************************
 
     while(1)
     {
-/// PT100 Temperatur Messung
-        ADMUX = 0x40;                                       //  --> PC0
-        ADCSRA |= 0x07;
-        ADCSRA |= (1<<ADEN);
-
-        ADCSRA |= (1<<ADSC);
-        while(ADCSRA & (1<<ADSC));
-        pt100_temp = ADC;                                   //  409,6Bit @ 0°C und 490,7Bit @ 100°C ===>>   1,233Bit/°C
+        ok = 0;
 
 /// Leitfähigkeitstest
         ADMUX = 0x41;                                       //  --> PC1
@@ -69,28 +65,40 @@ int main(void)
 
         ADCSRA |= (1<<ADSC);
         while(ADCSRA & (1<<ADSC));
-        leitfaehig = ADC;                                   //
+        leitfaehig = ADC;                                   //  Leitfähigkeit == 1023 ===>> Achtung Wasser!
 
+        if(leitfaehig <= 820)
+        {
+            ok++;
+        }
+        else
+            ssr_off();
 
-///  Ausgabe in MyMonitor
-        //itoa(pt100_temp,temperatur,10);
-        //_puts(temperatur);
+/// PT100 Temperatur Messung
+        ADMUX = 0x40;                                       //  --> PC0
+        ADCSRA |= 0x07;
+        ADCSRA |= (1<<ADEN);
 
-        PORTD ^= (1<<PD4);
-        PORTD ^= (1<<PD5);
-        _delay_ms(250);
+        ADCSRA |= (1<<ADSC);
+        while(ADCSRA & (1<<ADSC));
+        pt100_temp = ADC;                                   //  409,6Bit @ 0°C und 490,7Bit @ 100°C ===>>   0,811°C/Bit
+
+        if(pt100_temp <= 474)                               // Temperatur unter 80°C
+        {
+            ok++;
+        }
+        else
+            ssr_off();
 
 ///  10us Trigger Impuls
         PORTD |= (1<<trigger);
         _delay_us(10);
         PORTD &= ~(1<<trigger);
-
 ///  Timer Setup
         TCNT1 = 0;
         TCCR1B = 0x41;                                      //  aktiv bei steigende Flanke, kein Prescaler
         TIFR1 = (1<<ICF1);                                  //  clear input Capture Flag
         TIFR1 = (1<<TOV1);                                  //  clear Timer Overflow Flag
-
 ///  Distanz berechnen (width of Echo by ICF)
         while((TIFR1 && (1<<ICF1)) == 0);                   //  Wartet auf steigende Flanke
         TCNT1 = 0;
@@ -98,17 +106,41 @@ int main(void)
         TIFR1 = (1<<ICF1);                                  //  clear ICF1
         TIFR1 = (1<<TOV1);                                  //  clear TOV1
         timer1_Overflow = 0;
-
 ///  Berechnung Distanz
         while((TIFR1 && (1<<ICF1)) == 0);                   //  Wartet auf fallende Flanke
         count = ICR1 + (65535 * timer1_Overflow);
-        distance = ((((uint64_t)count/100)-375)*11)+400;    //  Messbereich: 4 bis 32cm
+        distance = ((((uint64_t)count/100)-375)*11)+400;    //  Messbereich: 4 bis 32cm ==>>    400 bis 3200
 
-        itoa(leitfaehig,distance_forOut,10);
-        //_delay_ms(200);
-        _puts(distance_forOut);
+        if((distance < 750) && (distance > 450))
+        {
+            ok++;
+        }
+        else
+            ssr_off();
+
+        if(ok == 3)
+        {
+            PORTC |= (1<<ssr1);
+            PORTC |= (1<<ssr2);
+        }
+
+/// Alarm blinken
+        else if(ok < 3)
+        {
+            PORTD ^= (1<<PD4);
+            PORTD ^= (1<<PD5);
+        }
+
+/// Delay
+        _delay_ms(200);
 
     }
 
     return 0;
+}
+
+void ssr_off(void)
+{
+    PORTC &= ~(1<<ssr1);
+    PORTC &= ~(1<<ssr2);
 }
